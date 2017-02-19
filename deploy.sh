c5ce502b21c5fc08f75f9fa034db7ca6b3bb8b82
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-02-19 00:42:16 +0000

    using new eb aws cli

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# deploy.sh
+#! /bin/bash
+
+SHA1=$1
+
+aws s3 cp ./release/efr-service-go.zip "s3://elasticbeanstalk-eu-west-1-696293867939/efr-service-go.zip"
+
+aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" \
+  --version-label $SHA1 --source-bundle S3Bucket="elasticbeanstalk-eu-west-1-696293867939",S3Key="efr-service-go.zip"
+
+
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name Default-Environment \
+    --version-label $SHA1
