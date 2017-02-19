a4579fb1d98f55e33b16994db8e328c2902dd563
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-02-19 00:55:54 +0000

    adding region

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ SHA1=$1
 
 aws s3 cp ./release/efr-service-go.zip "s3://elasticbeanstalk-eu-west-1-696293867939/efr-service-go.zip"
 
-aws elasticbeanstalk create-application-version --application-name "My First Elastic Beanstalk Application" \
+aws elasticbeanstalk create-application-version --region=eu-west-1 --application-name "My First Elastic Beanstalk Application" \
   --version-label $SHA1 --source-bundle S3Bucket="elasticbeanstalk-eu-west-1-696293867939",S3Key="efr-service-go.zip"
 
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name Default-Environment \
-    --version-label $SHA1 --region=eu-west-1
+aws elasticbeanstalk update-environment --region=eu-west-1 --environment-name Default-Environment \
+    --version-label $SHA1
